e23d63962c721c979812556041954a454aaa3caa
ssmiley <ssmiley@ecwise.com>
2014-06-11 21:08:05 +0000

    Added rightscale_marker to default.rb

--- cookbooks/hello_world/metadata.rb
+++ cookbooks/hello_world/metadata.rb
@@ -6,4 +6,6 @@ description      'Installs/Configures hello_world'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends "rightscale"
+
 recipe "hello_world::default","Prints hello world output"
--- cookbooks/hello_world/recipes/default.rb
+++ cookbooks/hello_world/recipes/default.rb
@@ -5,4 +5,5 @@
 # Copyright 2014, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
+rightscale_marker
 log "Hello World! You should try Chef with RightScale!"
