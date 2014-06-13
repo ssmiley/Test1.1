2eb0287da789ae9267a5e80fff69f4115b81dc6e
ssmiley <ssmiley@ecwise.com>
2014-06-13 23:01:19 +0000

    Converted the hobby attribute into an input

--- cookbooks/about_me/metadata.rb
+++ cookbooks/about_me/metadata.rb
@@ -9,3 +9,10 @@ version          '0.1.0'
 depends "rightscale"
 
 recipe "about_me::default","Prints my first name and several of my favorite things as output."
+
+# Required #
+attribute "about_me/favorite/hobby",
+   :display_name => "Hobby",
+   :description => "My favorite hobby.",
+   :required => "required",
+   :recipes => ["about_me::default"]
