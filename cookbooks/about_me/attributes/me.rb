066e8599806a89604887c3b690fe05aa883b63cf
ssmiley <ssmiley@ecwise.com>
2014-06-13 21:30:25 +0000

    Added the about_me cookbook

---
+++ cookbooks/about_me/attributes/me.rb
@@ -0,0 +1,4 @@
+set[:about_me][:first_name] = "<INSERT_YOUR_NAME_HERE>"
+default[:about_me][:favorite][:food] = "pizza"
+default[:about_me][:favorite][:drink] = "water"
+default[:about_me][:favorite][:hobby] = "running"
