85042295e54761a605741ce9ef126a7de12e43a2
ssmiley <ssmiley@ecwise.com>
2014-09-12 02:18:51 +0000

    Added awscli::aws_config recipe to provide dynamic attributes.

--- cookbooks/awscli/metadata.rb
+++ cookbooks/awscli/metadata.rb
@@ -7,3 +7,25 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 recipe "awscli::awscli","Installs the AWS CLI"
+recipe "awscli::aws_config","AWS credentials, and region"
+
+# Reguired Input #
+attribute "awscli/access_key",
+  :display_name => "Access Key",
+  :description => "The AWS access key for your account.",
+  :required => "required",
+  :recipes => ["awscli::aws_config"]
+
+# Required Input #
+attribute "awscli/secret_access_key",
+  :display_name => "Secret Access Key",
+  :description => "The AWS secret access key for your account.",
+  :required => "required",
+  :recipes => ["awscli::aws_config"]
+
+# Required Input #
+attribute "awscli/region",
+  :display_name => "AWS Region",
+  :description => "The AWS region where your instances are running",
+  :required => "required",
+  :recipes => ['awscli::aws_config']
---
+++ cookbooks/awscli/recipes/aws_config.rb
@@ -0,0 +1,3 @@
+access_key =node[:awscli][:access_key]
+secret_acess_key = node[:awscli][:secret_access_key]
+region = node[:awscli][:region]
