685841a73834bec1662e2984f6aee19b4cf4ad9c
ssmiley <ssmiley@ecwise.com>
2014-10-01 23:53:44 +0000

    Converted user_home to a rightscale input.

--- cookbooks/awscli/metadata.rb
+++ cookbooks/awscli/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures awscli'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 recipe "awscli::awscli","Installs the AWS CLI"
 recipe "awscli::aws_configure","Configures the AWS CLI"
@@ -28,5 +28,12 @@ attribute "awscli/secret_access_key",
 attribute "awscli/region",
   :display_name => "AWS Region",
   :description => "The AWS region where your instances are running, ex. us-west-1",
+  :required => "required",
+  :recipes => ['awscli::aws_config']
+
+# Required Input #
+ attribute "awscli/user_home",
+  :display_name => "User Home",
+  :description => "The user's home directory where the aws config file will be stored.",
   :required => "required",
   :recipes => ['awscli::aws_config']
--- cookbooks/awscli/recipes/aws_config.rb
+++ cookbooks/awscli/recipes/aws_config.rb
@@ -1,5 +1,4 @@
 access_key =node[:awscli][:access_key]
 secret_acess_key = node[:awscli][:secret_access_key]
 region = node[:awscli][:region]
-
-default[:awscli][:user_home] = "/root"
+user_home = node[:awscli][:user_home]
