6d578977dbe76288e444fc8bf5a605be5716c454
ssmiley <ssmiley@ecwise.com>
2014-10-01 23:17:08 +0000

    Updated the version to 0.1.1

--- cookbooks/awscli/metadata.rb
+++ cookbooks/awscli/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures awscli'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 recipe "awscli::awscli","Installs the AWS CLI"
 recipe "awscli::aws_configure","Configures the AWS CLI"
